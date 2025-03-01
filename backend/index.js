ea59822b94fb9d44f1a896fd6a726acd867043ea
Navjot Singh <navjotsingh16082003@gmail.com>
2025-03-01 23:12:57 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,26 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import qrRoutes from "./routes/qrRoutes.js";
 
-dotenv.config({ path: "./.env" });
+dotenv.config({ path: './.env' }); 
 
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONTEND_URI,
+  credentials: true,  
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-const allowedOrigins = [
-  "https://dytes-frontend.vercel.app", // Deployed frontend
-  "http://localhost:5173", // For local testing
-];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("CORS not allowed"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-app.options("*", cors());
-
-
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/qrcode", qrRoutes);
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+const PORT = process.env.PORT;
+
+app.listen(PORT || 8000, () => console.log(`server started at http://localhost:${PORT}`) );
